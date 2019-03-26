c6b828895e27fc2ad0ea4c2da9e3b5347e78b552
EdwardAndress <eddie_andress@hotmail.com>
2019-03-26 13:53:45 +0000

    Reorganises better_benchmarks.rb for readability

--- better_benchmarks.rb
+++ better_benchmarks.rb
@@ -5,12 +5,30 @@ require 'squid'
 class BetterBenchmarks
   class << self
 
-    def in_microseconds(seconds)
-      seconds * 1000 * 1000
+    def time_vs_array_size(method, array)
+      test_arrays = build_test_arrays(array)
+
+      data = {}
+
+      test_arrays.each do |test_array|
+        data[test_array.length.to_i] = mean_benchmark(method, test_array)
+      end
+
+      Prawn::Document.generate "#{method.to_s}#{Time.now}.pdf" do
+        chart({method.to_s => data}, {type: :point})
+      end
     end
 
-    def mean(nums_array)
-      nums_array.sum / nums_array.length.to_f
+    def build_test_arrays(array)
+      test_arrays = []
+      step_size = array.length / 5
+
+      5.times do
+        test_arrays << array
+        array = array.drop(step_size)
+      end
+
+      test_arrays.reverse
     end
 
     def mean_benchmark(method, array)
@@ -26,30 +44,12 @@ class BetterBenchmarks
       in_microseconds(mean)
     end
 
-    def build_test_arrays(array)
-      test_arrays = []
-      step_size = array.length / 5
-
-      5.times do
-        test_arrays << array
-        array = array.drop(step_size)
-      end
-
-      test_arrays.reverse
+    def in_microseconds(seconds)
+      seconds * 1000 * 1000
     end
 
-    def time_vs_array_size(method, array)
-      test_arrays = build_test_arrays(array)
-
-      data = {}
-
-      test_arrays.each do |test_array|
-        data[test_array.length.to_i] = mean_benchmark(method, test_array)
-      end
-
-      Prawn::Document.generate "#{method.to_s}#{Time.now}.pdf" do
-        chart({method.to_s => data}, {type: :point})
-      end
+    def mean(nums_array)
+      nums_array.sum / nums_array.length.to_f
     end
   end
 end
